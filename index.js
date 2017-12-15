1b08ab5f7b9b9e8fc86abaa9801f9e0a284af4c8
Max Kelly <maxleekelly@gmail.com>
2017-12-15 16:14:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -51,17 +51,28 @@ function total() {
 
 function removeFromCart(item) {
   // write your code here
+  var k = 0;
   for (let i = 0; i < cart.length; i++) {
-    if (cart[i].hasOwnProperty(item) === true) {
-      delete cart[i];
+    if (cart[i].hasOwnProperty(item)) {
+      k = 1;
+      cart.splice(i, 1);
       return cart;
     } else {
-      console.log("That item is not in your cart.");
-      return cart;
+      k = 0;
     }
   }
+
+  if (k === 0) {
+    console.log("That item is not in your cart.");
+    return cart;
+  }
+  
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  var card = [];
+  card.push(cardNumber);
+  if (card.length === 0) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
 }
