8b3f6d5785f6167ec0e4debcd5f8e4d87d755d65
Max Kelly <maxleekelly@gmail.com>
2017-12-15 16:19:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,7 +70,6 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if (cardNumber.length === 0) {
-    console.log("Sorry, we don't have a credit card on file for you.");
-  }
+  console.log(`Your total cost is ${total()}, which will be charged to the card ${cardNumber}.`);
+  
 }
