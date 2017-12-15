c4a849aa90ca057cd23c0982e9f668914ff03952
Max Kelly <maxleekelly@gmail.com>
2017-12-15 16:26:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -70,6 +70,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  
+  if (isNaN(cardNumber)) {
+    console.log("Sorry, we don't have a credit card on file for you.");
+  }
   
 }
